d4e4fd5edffb79b98cebc9b3e0b9dd91877010ef
Elliot Lee <drelliotlee@gmail.com>
2024-01-15 17:29:45 -0500

    dags actually download and convert successfully

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import datetime
 import pandas as pd
 import pyarrow.parquet as pq
@@ -11,18 +12,23 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
-AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+# AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+# AIRFLOW_HOME = "/opt/airflow/"
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
+execution_date = datetime.now().strftime('%Y-%m')
 FILE_URL = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
-PARQUET_FILENAME = AIRFLOW_HOME + '/{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
-CSV_FILENAME = AIRFLOW_HOME + '/{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+PARQUET_FILENAME = f'{execution_date}.parquet'
+CSV_FILENAME = f'{execution_date}.csv'
+
 with DAG(
-	dag_id="elliots_first_dag",
-	schedule_interval="@daily",
-	start_date=datetime(2021,1,1),
+	dag_id="elliots_first_dag10",
+	schedule_interval="@monthly",
+	# schedule_interval=None,
+	start_date=datetime(2022,2,1),
+	end_date=datetime(2022,3,1),
 	catchup=True
 ) as dag:
 	
@@ -33,7 +39,11 @@ with DAG(
 
 	@task
 	def convert_to_csv():
-		table = pq.ParquetDataset(PARQUET_FILENAME).read()
+		# logging.info('airflow_home is:' + AIRFLOW_HOME)
+		logging.info('parquet_filename is:' + PARQUET_FILENAME)
+		logging.info('execution_date is:' + execution_date)
+		dataset = pq.ParquetDataset(PARQUET_FILENAME)
+		table = dataset.read()
 		df = table.to_pandas()
 		df.to_csv(CSV_FILENAME, index=False)
 	
